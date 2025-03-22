c7e18422b70270c0022b86e7ea8c586b332c020b
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-03-22 22:09:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your code here
+        document.getElementById("evaluatedText").addEventListener("input", function() {
+            document.getElementById("letterCount").innerText = this.value.length;
+        });
